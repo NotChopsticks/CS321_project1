6bac4cfaa67bf75828fe35e2138b23156d649cad
Adam Pittman <adam.w.pittman@gmail.com>
2020-01-24 18:11:37 -0600

    Update MusicPlayer.java

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -11,6 +11,19 @@ import java.util.Random;
 
 
 public class MusicPlayer {
+<<<<<<< Updated upstream
+=======
+    ArrayList<song> MusicLibrary;
+    ArrayList<song> queue;
+    ArrayList<song> recommended;
+    ArrayList<song> liked;
+    ArrayList<song> disliked;
+    ArrayList<song> shuffledQueue;
+    Random shuffler = null;
+    song currentSong;
+
+
+>>>>>>> Stashed changes
 
     public MusicPlayer (){
         ArrayList<song> MusicLibrary;
@@ -22,7 +35,20 @@ public class MusicPlayer {
 
     }
 
+    public void shuffleQueue() {
+        ArrayList<song> originalQueue = queue;
+        
+        int index;
 
+        for (int i = 0; i < queue.size(); i++) {
+            index = shuffler.nextInt(originalQueue.size());
+            shuffledQueue.add(originalQueue.get(index));
+            originalQueue.remove(index);
+        }
+        
+        queue = shuffledQueue;
+        
+    }
 
 
 
