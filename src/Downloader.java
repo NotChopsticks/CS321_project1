7a860e8292a5d787dc5d14f62a1a5dffb5a9a81c
NotChopsticks <Notchopsticks@github.com>
2020-01-24 19:04:01 -0600

    added println statements

---
+++ src/Downloader.java
@@ -0,0 +1,11 @@
+public class Downloader {
+
+    public Downloader() {
+
+    }
+
+    public void downloadSong(song newSong){
+        System.out.println("Downloading "+ newSong.songName);
+    }
+
+}
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -29,31 +29,36 @@ public class MusicPlayer {
 
     public void addToQueue(song newSong) {
         queue.add(newSong);
+        System.out.println("Songs added to queue");
     }
 
     public void addToQueue(ArrayList<song> playlist) {
         queue.addAll(playlist);
+        System.out.println("Songs added to queue");
     }
 
     public void playSong(song newSong) {
         currentSong = newSong;
+        System.out.println("Now Playing "+ currentSong.songName + ".");
     }
 
     public void likeOrDislikeSong() {
        currentSong.liked = !currentSong.liked;
+       System.out.println("Song Liked");
     }
 
     public void nextSong() {
         playedSongs.add(currentSong);
         currentSong = queue.get(0);
         queue.remove(0);
+        System.out.println("Now Playing "+ currentSong.songName + ".");
     }
 
     public void prevSong() {
         queue.add(0, playedSongs.get(playedSongs.size()));
         currentSong = playedSongs.get(playedSongs.size());
         playedSongs.remove(currentSong);
-
+        System.out.println("Now Playing "+ currentSong.songName + ".");
     }
 
     public void shuffleQueue() {
@@ -68,6 +73,7 @@ public class MusicPlayer {
         }
         
         queue = shuffledQueue;
+        System.out.println("Queue Shuffled");
         
     }
 
