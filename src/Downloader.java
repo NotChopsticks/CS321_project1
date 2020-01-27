3629db3b8c0297c49e85e7fa1679080f58a058d2
Adam Pittman <adam.w.pittman@gmail.com>
2020-01-27 13:48:02 -0600

    Fixed crashing with download

--- src/Downloader.java
+++ src/Downloader.java
@@ -10,7 +10,12 @@ public class Downloader {
     // method for downloading a song
     // simply outputs the status of a song being downloaded to the terminal
     public void downloadSong(song newSong){
+        if (newSong != null) {
         System.out.println("Downloading "+ newSong.songName);
+        }
+        else {
+            System.out.println("There is no song currently selected to download");
+        }
     }
 
 }
